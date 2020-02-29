8bde00f740eb4cce1b5137b0b9c347ff6aaad79e
GK4mil <48480393+GK4mil@users.noreply.github.com>
2020-02-29 15:37:35 +0100

    Delete db_connect.php

--- DSM/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-//System powstał na Wydziale Informatyki i Nauk o Żywności PWSIiP w Łomży
-$MYSQL_STRING="mysql:host=adres_bazy_danych;dbname=nazwa_bazy_danych";
-$USERNAME="";
-$PASSWORD="";
-?>
